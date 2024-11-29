53cbbde4cbc63498a1c33ec0bc017c46b2f669de
Сергей Субботин <zarodiny@yandex.ru>
2024-11-29 11:57:37 +1000

    feat: добавляет панель

--- app/components/VdsPanel/VdsPanel.jsx
+++ app/components/VdsPanel/VdsPanel.jsx
@@ -1,9 +1,152 @@
+'use client'
 
 import Image from "next/image";
 import styles from "./vdspanel.module.css";
 
 export default function VdsPanel() {
+    // Повторное произношение текста
+    function speachtxt(txt) {
+        speechSynthesis.speak(new SpeechSynthesisUtterance(txt));
+    }
+
+    // Уменьшение шрифта
+    const fontDecrease = () => {
+        const currentFontSize = parseInt(window.getComputedStyle(document.body).fontSize);
+        const newFontSize = currentFontSize - 2;
+        document.body.style.fontSize = newFontSize + 'px';
+        speachtxt("размер шрифта уменьшен");
+    }
+    // Увеличение шрифта
+    const fontIncrease = () => {
+        const currentFontSize = parseInt(window.getComputedStyle(document.body).fontSize);
+        const newFontSize = currentFontSize + 2;
+        document.body.style.fontSize = newFontSize + 'px';
+        speachtxt("размер шрифта увеличен");
+    }
+
+    // Функция для смены темы
+    function changeTheme(allStyles, newTheme) {
+        document.body.classList.remove(
+            allStyles.theme_white,
+            allStyles.theme_black,
+            allStyles.theme_blue,
+            allStyles.theme_yellow,
+            allStyles.theme_green
+        );
+        document.body.classList.add(newTheme);
+    }
+
+    const changeThemeWhite = () => {
+        changeTheme(styles, styles.theme_white);
+        speachtxt("цвет сайта черным по белому");
+    }
+    const changeThemeBlack = () => {
+        changeTheme(styles, styles.theme_black);
+        speachtxt("цвет сайта белым по черному");
+    }
+    const changeThemeBlue = () => {
+        changeTheme(styles, styles.theme_blue);
+        speachtxt("цвет сайта темно синим по голубому");
+    }
+    const changeThemeYellow = () => {
+        changeTheme(styles, styles.theme_yellow);
+        speachtxt("цвет сайта темно коричневым по бежевому");
+    }
+    const changeThemeGreen = () => {
+        changeTheme(styles, styles.theme_green);
+        speachtxt("цвет сайта зеленым по темно коричневому");
+    }
+
+    // Работа с изображениями
+    const removeImages = (evt) => {
+        speachtxt("изображения выключены");
+
+        const images = document.querySelectorAll('.dsv-image');
+        images.forEach(image => {
+            image.style.display = 'none';
+            const alt = image.getAttribute('alt');
+
+            if (alt !== '') {
+                const div = document.createElement('div');
+                div.className = 'dsv-image-div';
+                div.textContent = alt;
+                image.parentNode.insertBefore(div, image.nextSibling);
+            }
+        });
+
+    }
+
     return (
-        <>222222222222222222222</>
+        <div className={styles.dsv_panel}>
+            <div>
+                <p className={styles.p}>Размер шрифта</p>
+                <div>
+                    <button
+                        className={styles.button}
+                        onClick={fontDecrease}
+                    >A-
+                    </button>
+                    <button
+                        className={styles.button}
+                        onClick={fontIncrease}
+                    >A+
+                    </button>
+                </div>
+            </div>
+
+            <div>
+                <p className={styles.p}>Цвета сайта</p>
+                <div>
+                    <button
+                        className={`${styles.button} ${styles.color_1}`}
+                        onClick={changeThemeWhite}
+                    >Ц</button>
+                    <button
+                        className={`${styles.button} ${styles.color_2}`}
+                        onClick={changeThemeBlack}
+                    >Ц</button>
+                    <button
+                        className={`${styles.button} ${styles.color_3}`}
+                        onClick={changeThemeBlue}
+                    >Ц</button>
+                    <button
+                        className={`${styles.button} ${styles.color_4}`}
+                        onClick={changeThemeYellow}
+                    >Ц</button>
+                    <button
+                        className={`${styles.button} ${styles.color_5}`}
+                        onClick={changeThemeGreen}
+                    >Ц</button>
+                </div>
+            </div>
+
+            <div>
+                <p className={styles.p}>Изображения</p>
+                <div>
+                    <button className={styles.button}>
+                        <Image src="/icons/image.svg" alt="" width={20} height={20} />
+                    </button>
+                    <button
+                        className={styles.button}
+                        onClick={removeImages}
+                    >
+                        <Image src="/icons/minus-circle.svg" alt="" width={20} height={20} />
+                    </button>
+                    <button className={styles.button}>
+                        <Image src="/icons/adjust.svg" alt="" width={20} height={20} />
+                    </button>
+
+                </div>
+            </div>
+
+            <div>
+                <p className={styles.p}>Настройки</p>
+                <div>
+                    <button id="btn-close-panel">
+                        Обычная версия сайта
+                    </button>
+                </div>
+            </div>
+        </div>
     )
 }
--- app/page.js
+++ app/page.js
@@ -15,7 +15,20 @@ export default function Home() {
       <Header />
       <main>
         <h1 className="visually-hidden">Дальневосточный центр непрерывного образования</h1>
-        main lorem25 wwwww
+        <Image
+          aria-hidden
+          src="/content/image-1.png"
+          alt="Изображение"
+          width={409}
+          height={195}
+          className="dsv-image"
+        />
+        <p className={styles.main_text}>Дальневосточный центр непрерывного образования</p>
+        <p className={styles.main_text_2}>Дальневосточный центр непрерывного образования</p>
+        <p>Дальневосточный центр непрерывного образования</p>
+        <p>Дальневосточный центр непрерывного образования</p>
+        <p>Дальневосточный центр непрерывного образования</p>
+        <p>Дальневосточный центр непрерывного образования</p>
       </main>
       <footer className={styles.footer}>
         <Image
