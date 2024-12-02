6dcc1ab656e0ed445bcbf23afead82fa9001bbf1
Сергей Субботин <zarodiny@yandex.ru>
2024-12-02 12:27:23 +1000

    feat: добавлят кнопку каталога

--- app/components/Catalog-button/CatalogButton.jsx
+++ app/components/Catalog-button/CatalogButton.jsx
@@ -1,21 +1,30 @@
 
+import { useState } from "react";
+
 import Image from "next/image";
 import styles from "./style.module.css";
 
-export default function CatalogButton({ setPanel, setPanelBtn }) {
+export default function CatalogButton() {
+    const [opened, setOpened] = useState(false);
+
+    const handleClick = () => {
+        setOpened(!opened);
+    }
 
     return (
         <button
-            className={styles.button}
+            aria-label={opened ? 'Закрыть меню' : 'Открыть меню'}
+            className={`${opened ? styles.button + ' ' + styles.active : styles.button}`}
+            onClick={handleClick}
         >
             <Image
                 aria-hidden
-                src="/icons/burger.svg"
+                src={`${opened ? '/icons/close.svg' : '/icons/burger.svg'}`}
                 alt="пин"
                 width={20}
                 height={17}
             />
-            <span className="">menu</span>
+            <span>Меню</span>
         </button>
     )
 }
