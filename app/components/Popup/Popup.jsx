dd181e9797cd546367b201468f27b41d0c55e264
kkirrin <khlebnikov-2016@mail.ru>
2024-12-03 16:18:20 +1000

    feat: компонент Form и стили

--- app/components/Popup/Popup.jsx
+++ app/components/Popup/Popup.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from './style.module.css';
+import { Form } from "../Form/Form";
 
 
 
@@ -11,23 +12,20 @@ export default function Popup({ active, setActive }) {
     return (
         <div className={`${styles.popup} ${active ? styles.popupActive : styles.popupNone}`} onClick={() => {setActive(false)}}>
             <div className={styles.popup__body}>
+                
                 <div className={styles.popup__content} onClick={e => e.stopPropagation()}>
-                    <p>У вас есть вопросы?</p>
-                    <p>
-                        Задайте их нашим специалистам
-                    </p>
-                    <div>
-                        <label>ФИО</label>
-                        <input type='text' placeholder='' />
-                    </div>
-                    <div>
-                        <label>Электронная почта / номер телефона</label>
-                        <input type='text' placeholder='' />
-                    </div>
-                    <div>
-                        <label>Ваш запрос</label>
-                        <textarea type='text' placeholder='' />
-                    </div>
+
+                    <button 
+                        className={styles.popup__close} 
+                        onClick={() => { setActive(false) }}
+                        >
+                        <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <rect x="2.80078" y="12.8408" width="14.1721" height="0.472405" rx="0.236202" transform="rotate(-45 2.80078 12.8408)" fill="white"/>
+                            <rect x="3.58789" y="2.80078" width="14.1721" height="0.472405" rx="0.236202" transform="rotate(45 3.58789 2.80078)" fill="white"/>
+                        </svg>
+                    </button>
+                    
+                    <Form />
                 </div>
             </div>
         </div>
