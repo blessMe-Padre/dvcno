d2d55256a3b4e4fb05149022a2cb0d38fa66ea51
kkirrin <khlebnikov-2016@mail.ru>
2024-12-03 14:57:53 +1000

    feat: добавляет компоненту Popup и логику

---
+++ app/components/Popup/Popup.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import styles from './style.module.css';
+
+
+
+export default function Popup({ active, setActive }) {
+    
+
+    // const [opened, setOpened] = useState();
+
+    return (
+        <div className={`${styles.popup} ${active ? styles.popupActive : styles.popupNone}`} onClick={() => setActive(false)}>
+            <div className={styles.popup__body} onClick={e => e.stopPropagation()}>
+                <div className={styles.popup__content}>
+                    <p>У вас есть вопросы?</p>
+                    <p>
+                        Задайте их нашим специалистам
+                    </p>
+                    <div>
+                        <label>ФИО</label>
+                        <input type='text' placeholder='' />
+                    </div>
+                    <div>
+                        <label>Электронная почта / номер телефона</label>
+                        <input type='text' placeholder='' />
+                    </div>
+                    <div>
+                        <label>Ваш запрос</label>
+                        <textarea type='text' placeholder='' />
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
