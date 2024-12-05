bd6c66c080fafe90062827266c18069ee874937b
kkirrin <khlebnikov-2016@mail.ru>
2024-12-05 16:50:57 +1000

    feat: свайпер

--- app/components/Slider/Slider.jsx
+++ app/components/Slider/Slider.jsx
@@ -11,10 +11,34 @@ import sliderBgBig from '../../../public/main-sliders/sliders-bg-1.png'
 import sliderBgMedium from '../../../public/main-sliders/sliders-bg-m-1.png'
 import sliderBgSmall from '../../../public/main-sliders/sliders-bg-s-1.png'
 
+import { mainSlidersData } from '../../../mock-data/sliders' 
+
+
 
 export default function Slider() {
 
     const [sliderBg, setSliderBg] = useState(sliderBgBig);
+
+    
+    const getALlSliders = async () => {
+        try {
+            const response = await fetch('../../mock-data/sliders.js');
+            
+            // if (response.ok) {
+            //     const mainSlidersData = response.data;
+            // }
+
+            return response.data;
+        } catch (error) {
+            console.error(`Error`, error);
+            // throw error;
+        }
+    };
+
+    useEffect(() => {
+        getALlSliders();
+    }, [])
+
 
     useEffect(() => {
         function handleResize() {
@@ -25,6 +49,7 @@ export default function Slider() {
                 setSliderBg(sliderBgMedium);
             }
             else if (window.innerWidth <= 480) {
+
                 setSliderBg(sliderBgSmall);
             }
         }
@@ -38,53 +63,72 @@ export default function Slider() {
     return (
         <section
             className={styles.section__swiper}
-        >
+            >
             <Swiper
                 spaceBetween={50}
                 slidesPerView={1}
-            >
-                <SwiperSlide 
-                    
                 >
-                    <div
-                        className={styles.slider} 
-                        style={{ 
-                            width: '100%', 
-                            height: "100%", 
-                            backgroundImage: `url(${sliderBg.src})`,
-                            backgroundPosition: 'center',
-                            backgroundRepeat: 'no-repeat',
-                            objectFit: 'cover',
-                            borderRadius: '20px'
-                        }}>
-                        <div className={styles.slider__wrapper__content}>
-                                
-                            <h2 className={styles.slider__title}>
-                                Основное общее образование в ДВЦНО
-                            </h2>
+                {
+                
+                    mainSlidersData && 
 
-                            <p className={styles.slider__desc}>
-                                Это второй уровень образования, цель которого - становление и формирование личности обучающегося, развитие его склонностей и интересов
-                            </p>
+                        mainSlidersData.map((item, index) => {
+                            return (
+                            // ПОДОГНАТЬ ВСЕ СЛАЙДЕРЫ ПОД ОДИН РАЗМЕР
+                            <SwiperSlide key={index}>
+                                <Image 
+                                    src={sliderBg}
+                                    width='100%'
+                                    height='100%'
+                                    alt='slider_bg'
+                                    className={styles.sliderBg}
 
-                            <ul className={styles.slider__list}>
-                                <li>Международная лингвистическая школа</li>
-                                <li>Академический колледж (АК)</li>
-                                <li>«Общеобразовательная школа для одарённых детей им. Н.Н. Дубинина»</li>
+                                />
+                                <div
+                                    className={styles.slider} 
+                                    >
+                                    <div className={styles.slider__wrapper__content}>
+                                            
+                                        <h2 className={styles.slider__title}>
+                                            {item.title}
+                                        </h2>
 
-                            </ul>
+                                        <p className={styles.slider__desc}>
+                                            {item.description}
+                                        </p>
 
-                            <button className={styles.slider__btn__more}>
-                                <p>Узнать подробнее</p>
-                                <svg width="19" height="18" viewBox="0 0 19 18" fill="#" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M4.05507 1.43907L17.1536 1.43888M17.1536 1.43888L17.1536 14.3511M17.1536 1.43888L1.93782 16.6546" stroke="#191830" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-                                </svg>
-                            </button>
-                        </div>
+                                        <ul className={styles.slider__list}>
+                                            {item.listItems.map((element, idx) => {
+                                                
+                                                // СДЕЛАТЬ ПРОВЕРКУ НА КОЛ_ВО ЭЛЕМЕНТОВ В СПИСКЕ И ДАТЬ КЛАСС
+                                                return (
+                                                    <li key={idx}>
+                                                        {element}
+                                                    </li>
+                                                )
+                                            })}
+                                            {/* <li>Международная лингвистическая школа</li>
+                                            <li>Академический колледж (АК)</li>
+                                            <li>«Общеобразовательная школа для одарённых детей им. Н.Н. Дубинина»</li> */}
 
-                    </div>
-                </SwiperSlide>
+                                        </ul>
 
+                                        {/* ДОБАВИТЬ ССЫЛКУ  */}
+                                        <button className={styles.slider__btn__more}>
+                                            <p>Узнать подробнее</p>
+                                            <svg width="19" height="18" viewBox="0 0 19 18" fill="#" xmlns="http://www.w3.org/2000/svg">
+                                                <path d="M4.05507 1.43907L17.1536 1.43888M17.1536 1.43888L17.1536 14.3511M17.1536 1.43888L1.93782 16.6546" stroke="#191830" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+                                            </svg>
+                                        </button>
+                                    </div>
+
+                                </div>
+                            </SwiperSlide>
+                        )
+                    })
+                }
+                
+                
             </Swiper>
         </section>
     );
