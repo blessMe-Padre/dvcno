be69084c3ca17279f0a4a105af1f545e87d4be4c
kkirrin <khlebnikov-2016@mail.ru>
2024-12-05 15:53:02 +1000

    feat: рефакторинг Slider

--- app/components/Slider/Slider.jsx
+++ app/components/Slider/Slider.jsx
@@ -1,125 +1,86 @@
 'use client'
 import { Swiper, SwiperSlide } from 'swiper/react';
 import styles from './style.module.css';
+import Image from 'next/image';
+
+import { useState, useEffect } from 'react';
+
 import 'swiper/css';
 
+import sliderBgBig from '../../../public/main-sliders/sliders-bg-1.png'
+import sliderBgMedium from '../../../public/main-sliders/sliders-bg-m-1.png'
+import sliderBgSmall from '../../../public/main-sliders/sliders-bg-s-1.png'
+
+
 export default function Slider() {
+
+    const [sliderBg, setSliderBg] = useState(sliderBgBig);
+
+    useEffect(() => {
+        function handleResize() {
+            if (window.innerWidth >= 1200) {
+                setSliderBg(sliderBgBig);
+            }
+            else if (window.innerWidth >= 769) {
+                setSliderBg(sliderBgMedium);
+            }
+            else if (window.innerWidth <= 480) {
+                setSliderBg(sliderBgSmall);
+            }
+        }
+
+        window.addEventListener('resize', handleResize);
+        handleResize()
+        return () => window.removeEventListener('resize', handleResize);
+
+    }, []);
+
     return (
-        <section>
+        <section
+            className={styles.section__swiper}
+        >
             <Swiper
                 spaceBetween={50}
-                slidesPerView={3}
-                onSlideChange={() => console.log('slide change')}
-                onSwiper={(swiper) => console.log(swiper)}
+                slidesPerView={1}
             >
-                <SwiperSlide className={styles.slider} style={{ backgroundImage: `../public/main-sliders/slider-bg-1.png` }}>
-                    <div>
+                <SwiperSlide 
+                    
+                >
+                    <div
+                        className={styles.slider} 
+                        style={{ 
+                            width: '100%', 
+                            height: "100%", 
+                            backgroundImage: `url(${sliderBg.src})`,
+                            backgroundPosition: 'center',
+                            backgroundRepeat: 'no-repeat',
+                            objectFit: 'cover',
+                            borderRadius: '20px'
+                        }}>
+                        <div className={styles.slider__wrapper__content}>
+                                
+                            <h2 className={styles.slider__title}>
+                                Основное общее образование в ДВЦНО
+                            </h2>
 
-                        <h2 className={styles.slider__title}>
-                            Основное общее образование в ДВЦНО
-                        </h2>
+                            <p className={styles.slider__desc}>
+                                Это второй уровень образования, цель которого - становление и формирование личности обучающегося, развитие его склонностей и интересов
+                            </p>
 
-                        <p className={styles.slider__desc}>
-                            Это второй уровень образования, цель которого - становление и формирование личности обучающегося, развитие его склонностей и интересов
-                        </p>
+                            <ul className={styles.slider__list}>
+                                <li>Международная лингвистическая школа</li>
+                                <li>Академический колледж (АК)</li>
+                                <li>«Общеобразовательная школа для одарённых детей им. Н.Н. Дубинина»</li>
 
-                        <ul className={styles.slider__list}>
-                            <li>Международная лингвистическая школа</li>
-                            <li>Академический колледж (АК)</li>
-                            <li>«Общеобразовательная школа для одарённых детей им. Н.Н. Дубинина»</li>
+                            </ul>
 
-                        </ul>
-
-                        <button>
-                            <p>Узнать подробнее</p>
-                            <svg width="19" height="18" viewBox="0 0 19 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M4.05507 1.43907L17.1536 1.43888M17.1536 1.43888L17.1536 14.3511M17.1536 1.43888L1.93782 16.6546" stroke="#191830" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-                            </svg>
-                        </button>
-
-                    </div>
-                </SwiperSlide>
-
-                <SwiperSlide className={styles.slider} style={{ backgroundImage: `../public/main-sliders/slider-bg-1.png` }}>
-                    <div>
-
-                        <h2 className={styles.slider__title}>
-                            Основное общее образование в ДВЦНО
-                        </h2>
-
-                        <p className={styles.slider__desc}>
-                            Это второй уровень образования, цель которого - становление и формирование личности обучающегося, развитие его склонностей и интересов
-                        </p>
-
-                        <ul className={styles.slider__list}>
-                            <li>Международная лингвистическая школа</li>
-                            <li>Академический колледж (АК)</li>
-                            <li>«Общеобразовательная школа для одарённых детей им. Н.Н. Дубинина»</li>
-
-                        </ul>
-
-                        <button>
-                            <p>Узнать подробнее</p>
-                            <svg width="19" height="18" viewBox="0 0 19 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M4.05507 1.43907L17.1536 1.43888M17.1536 1.43888L17.1536 14.3511M17.1536 1.43888L1.93782 16.6546" stroke="#191830" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-                            </svg>
-                        </button>
-
-                    </div>
-                </SwiperSlide>
-
-                <SwiperSlide className={styles.slider} style={{ backgroundImage: `../public/main-sliders/slider-bg-1.png` }}>
-                    <div>
-
-                        <h2 className={styles.slider__title}>
-                            Основное общее образование в ДВЦНО
-                        </h2>
-
-                        <p className={styles.slider__desc}>
-                            Это второй уровень образования, цель которого - становление и формирование личности обучающегося, развитие его склонностей и интересов
-                        </p>
-
-                        <ul className={styles.slider__list}>
-                            <li>Международная лингвистическая школа</li>
-                            <li>Академический колледж (АК)</li>
-                            <li>«Общеобразовательная школа для одарённых детей им. Н.Н. Дубинина»</li>
-
-                        </ul>
-
-                        <button>
-                            <p>Узнать подробнее</p>
-                            <svg width="19" height="18" viewBox="0 0 19 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M4.05507 1.43907L17.1536 1.43888M17.1536 1.43888L17.1536 14.3511M17.1536 1.43888L1.93782 16.6546" stroke="#191830" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-                            </svg>
-                        </button>
-
-                    </div>
-                </SwiperSlide>
-
-                <SwiperSlide className={styles.slider} style={{ backgroundImage: `../public/main-sliders/slider-bg-1.png` }}>
-                    <div>
-
-                        <h2 className={styles.slider__title}>
-                            Основное общее образование в ДВЦНО
-                        </h2>
-
-                        <p className={styles.slider__desc}>
-                            Это второй уровень образования, цель которого - становление и формирование личности обучающегося, развитие его склонностей и интересов
-                        </p>
-
-                        <ul className={styles.slider__list}>
-                            <li>Международная лингвистическая школа</li>
-                            <li>Академический колледж (АК)</li>
-                            <li>«Общеобразовательная школа для одарённых детей им. Н.Н. Дубинина»</li>
-
-                        </ul>
-
-                        <button>
-                            <p>Узнать подробнее</p>
-                            <svg width="19" height="18" viewBox="0 0 19 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M4.05507 1.43907L17.1536 1.43888M17.1536 1.43888L17.1536 14.3511M17.1536 1.43888L1.93782 16.6546" stroke="#191830" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-                            </svg>
-                        </button>
+                            <button className={styles.slider__btn__more}>
+                                <p>Узнать подробнее</p>
+                                <svg width="19" height="18" viewBox="0 0 19 18" fill="#" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M4.05507 1.43907L17.1536 1.43888M17.1536 1.43888L17.1536 14.3511M17.1536 1.43888L1.93782 16.6546" stroke="#191830" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+                                </svg>
+                            </button>
+                        </div>
 
                     </div>
                 </SwiperSlide>
--- app/page.js
+++ app/page.js
@@ -18,21 +18,7 @@ export default function Home() {
       <Header />
       <main>
         <h1 className="visually-hidden">Дальневосточный центр непрерывного образования</h1>
-        <Image
-          aria-hidden
-          src="/content/image-1.png"
-          alt="Изображение"
-          width={409}
-          height={195}
-          className="dsv-image"
-        />
-        <p className={styles.main_text}>Дальневосточный центр непрерывного образования</p>
-        <p className={styles.main_text_2}>Дальневосточный центр непрерывного образования</p>
-        <p>Дальневосточный центр непрерывного образования</p>
-        <p>Дальневосточный центр непрерывного образования</p>
-        <p>Дальневосточный центр непрерывного образования</p>
-        <p>Дальневосточный центр непрерывного образования</p>
-
+        
         <Slider />
       </main>
 
