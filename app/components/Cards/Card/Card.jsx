940f28c72a62eb2ac06b94a76b917149895a7cc1
Сергей Субботин <zarodiny@yandex.ru>
2024-12-06 14:22:10 +1000

    fa: добавляет серверный компонент about

--- app/components/Cards/Card/Card.jsx
+++ app/components/Cards/Card/Card.jsx
@@ -15,7 +15,7 @@ export default function Card({ image, link, description, }) {
                 />
             </div>
             <Link href={link} className={styles.card_footer}>
-                <div className="flex flex-0">
+                <div className={styles.card_link_wrapper}>
                     <p className={styles.text}>{description}</p>
                     <div className={styles.link}>
                         <Image
