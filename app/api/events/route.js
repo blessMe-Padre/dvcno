a007dc1eb18f1756751c85127c1b8c120a133e45
Сергей Субботин <zarodiny@yandex.ru>
2024-12-11 13:18:41 +1000

    feat: добавляет роутинг events

--- app/api/events/route.js
+++ app/api/events/route.js
@@ -1,7 +1,6 @@
 import { eventsData } from "@/app/api/events/events-data";
 
 export async function GET() {
-    console.log(eventsData);
     const res = eventsData;
 
     return Response.json(res);
--- app/pages/event/[slug]/page.jsx
+++ app/pages/event/[slug]/page.jsx
@@ -1,7 +1,38 @@
-import React from 'react'
+import getEventsBySlug from '@/app/utils/getEventBySlug';
+import getAllEventSlugs from '@/app/utils/getAllEventSlugs';
+import { Metadata } from 'next';
+import { notFound } from 'next/navigation';
 
-export default function page() {
+// На данный момент страницы генерируются по SSR
+// https://nextjs.org/docs/app/api-reference/functions/generate-static-params
+
+export const metadata = {
+    title: "Событие",
+    description: "Описание события",
+};
+
+export async function generateStaticParams() {
+    const slugs = await getAllEventSlugs();
+    console.log(slugs);
+
+    return slugs.map((post) => ({
+        slug: post.slug,
+    }))
+}
+
+export default async function EventPage({ params }) {
+    const { slug } = await params;
+    const page = await getEventsBySlug(slug);
+
+    if (!page) {
+        notFound();
+    }
+
     return (
-        <div>Динамический роути</div>
-    )
+        <>
+            <div>Динамический роути</div>
+            <div>Страница с слагом {slug}</div>
+            <div>{page ? JSON.stringify(page) : 'Событие не найдено'}</div>
+        </>
+    );
 }
