94e697aa566e90a861b80abac1d2c1275087cdd5
kkirrin <khlebnikov-2016@mail.ru>
2024-12-12 15:26:41 +1000

    feat: делает динамический роутинг для News

---
+++ app/pages/news/[slug]/page.jsx
@@ -0,0 +1,40 @@
+import getAllNewsBySlug from '@/app/utils/getAllNewsSlugs';
+import { notFound } from 'next/navigation';
+
+// На данный момент страницы генерируются по SSR
+// https://nextjs.org/docs/app/api-reference/functions/generate-static-params
+
+// https://www.youtube.com/watch?v=2svgA1O4fO0&ab_channel=ITMATEPK
+
+export const metadata = {
+    title: "Новости",
+    description: "Описание новостей",
+};
+export default async function EventPage({ params }) {
+    const { slug } = await params;
+    const page = await getAllNewsBySlug(slug);
+
+    if (!page) {
+        // notFound();
+    }
+
+    return (
+        <>
+            <div>Динамический роути</div>
+            <div>Страница с слагом {slug}</div>
+            <div>{page ? JSON.stringify(page) : 'Событие не найдено'}</div>
+        </>
+    );
+}
+
+export async function generateStaticParams() {
+    const posts = await fetch('http://localhost:3000/api/news').then((res) => res.json());
+    console.log(posts)
+    const res = posts.map((post) => ({
+        slug: post.title,
+    }))
+    console.log(res);
+
+    return res;
+}
+
