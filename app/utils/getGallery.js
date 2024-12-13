39e5505e92c3939f59c78c1b385aa442e11ae46f
Сергей Субботин <zarodiny@yandex.ru>
2024-12-13 17:47:04 +1000

    feat: добавляет адаптив галереи

---
+++ app/utils/getGallery.js
@@ -0,0 +1,17 @@
+const getGallery = async () => {
+    try {
+        const res = await fetch('http://localhost:3000/api/gallery/', {
+            next: { revalidate: 10 }
+        });
+        if (!res.ok) {
+            throw new Error(`Ошибка HTTP: ${res.status}`);
+        }
+        const result = await res.json();
+        return result;
+    } catch (error) {
+        console.error("Ошибка при загрузке:", error);
+        return [];
+    }
+};
+
+export default getGallery;
