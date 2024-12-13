88c853cd30fd37e65b80b47a23a1575c1d9cf12b
Сергей Субботин <zarodiny@yandex.ru>
2024-12-13 17:47:04 +1000

    feat: добавляет адаптив галереи

--- app/sections/Gallery/Gallery.jsx
+++ app/sections/Gallery/Gallery.jsx
@@ -13,8 +13,22 @@ import Link from "next/link";
 import PhotoSwipeLightbox from 'photoswipe/lightbox';
 import 'photoswipe/style.css';
 
+import getGallery from '@/app/utils/getGallery';
+
 
 function Gallery() {
+
+    const [data, setData] = useState();
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await getGallery();
+            setData(result);
+        };
+
+        fetchData();
+
+    }, []);
 
     useEffect(() => {
         let lightbox = new PhotoSwipeLightbox({
@@ -36,63 +50,49 @@ function Gallery() {
                 <div className={styles.wrapper}>
                     <h2 className={styles.title}>галерея</h2>
                     <div className='pswp-gallery' id='main-gallery'>
-                        <Swiper
-                            spaceBetween={20}
-                            slidesPerView={2}
-                            modules={[Navigation]}
-                            loop={true}
-                            breakpoints={{
-                                320: {
-                                    slidesPerView: 1,
-                                },
-                                768: {
-                                    slidesPerView: 2,
-                                },
-                            }}
-                            className="swiper-mobile"
-                        >
-                            <SwiperSlide>
-                                <a
-                                    href={'/gallery/image-1.png'}
-                                    data-pswp-width={800}
-                                    data-pswp-height={800}
-                                    key={'#main-gallery' + '-' + 1}
-                                    target='_blank'
-                                    rel="noreferrer"
-                                    className={`${styles.img_wrapper} dsv-image`}
-                                >
-                                    <img src={'/gallery/image-1.png'} alt="" width={708} height={450} />
-                                </a>
-                            </SwiperSlide>
-                            <SwiperSlide>
-                                <a
-                                    href={'/gallery/image-2.png'}
-                                    data-pswp-width={800}
-                                    data-pswp-height={800}
-                                    key={'main-gallery' + '-' + 2}
-                                    target='_blank'
-                                    rel="noreferrer"
-                                    className={`${styles.img_wrapper} dsv-image`}
-                                >
-                                    <img src={'/gallery/image-2.png'} alt="" width={708} height={450} />
-                                </a>
-                            </SwiperSlide>
-                            <SwiperSlide>
-                                <a
-                                    href={'/gallery/image-1.png'}
-                                    data-pswp-width={800}
-                                    data-pswp-height={800}
-                                    key={'#main-gallery' + '-' + 3}
-                                    target='_blank'
-                                    rel="noreferrer"
-                                    className={`${styles.img_wrapper} dsv-image`}
-                                >
-                                    <img src={'/gallery/image-1.png'} alt="" width={708} height={450} />
-                                </a>
-                            </SwiperSlide>
 
-                            <SwiperNavButtons addClass="swiper_nav_btns" />
-                        </Swiper>
+                        {data && data.length > 0 ? (
+                            <Swiper
+                                spaceBetween={20}
+                                slidesPerView={2}
+                                modules={[Navigation]}
+                                loop={true}
+                                breakpoints={{
+                                    320: {
+                                        slidesPerView: 1,
+                                    },
+                                    768: {
+                                        slidesPerView: 2,
+                                    },
+                                }}
+                                className="swiper-mobile"
+                            >
+                                {data.map((image, index) => (
+
+                                    <SwiperSlide key={index}>
+                                        <a
+                                            href={image.largeURL}
+                                            data-pswp-width={image.width}
+                                            data-pswp-height={image.height}
+                                            key={'#main-gallery' + '-' + 1}
+                                            target='_blank'
+                                            rel="noreferrer"
+                                            className={`${styles.img_wrapper} dsv-image`}
+                                        >
+                                            <img src={image.largeURL} alt="" width={'100%'} height={'100%'} />
+                                        </a>
+                                    </SwiperSlide>
+                                ))}
+                                <SwiperNavButtons addClass="swiper_nav_btns" />
+                            </Swiper>
+
+                        )
+                            : (
+                                <div>
+                                    Загрузка данных...
+                                </div>
+                            )
+                        }
                     </div>
                 </div>
             </div>
