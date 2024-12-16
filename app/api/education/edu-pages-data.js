bdb9293b2bb242782c8e26a95961a68014524563
Сергей Субботин <zarodiny@yandex.ru>
2024-12-16 16:38:23 +1000

    feat: добавляет страницу образование

---
+++ app/api/education/edu-pages-data.js
@@ -0,0 +1,38 @@
+export const educationPages = [
+    {
+        id: 1,
+        image: '/about/image-1.png',
+        description: 'Начальная школа – детский сад «Классическая европейская прогимназия» (НШДС "КЕП")',
+        link: 'https://odarenok.su',
+    },
+    {
+        id: 2,
+        image: '/about/image-2.png',
+        description: 'Начальная общеобразовательная школа-детский сад "Восточная школа" (НОШДС «ВШ»)',
+        link: 'vostok@vvsu.ru',
+    },
+    {
+        id: 3,
+        image: '/about/image-3.png',
+        description: 'Международная лингвистическая школа (МЛШ)',
+        link: 'https://mlsh.ru/',
+    },
+    {
+        id: 4,
+        image: '/about/image-4.png',
+        description: 'Академический колледж (АК)',
+        link: 'https://ac.dvcno.ru',
+    },
+    {
+        id: 5,
+        image: '/about/image-5.png',
+        description: '«Общеобразовательная школа для одарённых детей им. Н.Н. Дубинина» (ШОД)',
+        link: 'https://giftedschool.dvcno.ru',
+    },
+    {
+        id: 6,
+        image: '',
+        description: '«Физкультурно-оздоровительный комплекс с бассейном «Лига спорта» (ФОК «Лига спорта»)',
+        link: 'https://sport-city-club.ru',
+    },
+];
