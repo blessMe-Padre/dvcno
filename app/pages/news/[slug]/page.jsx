4fb06635d3b350ede50dcf1ae1a8e8b24e778772
kkirrin <khlebnikov-2016@mail.ru>
2024-12-17 15:26:20 +1000

    feat: карточка news

--- app/pages/news/[slug]/page.jsx
+++ app/pages/news/[slug]/page.jsx
@@ -1,24 +1,75 @@
 import getNewsBySlug from '@/app/utils//getNewsBySlug';
+import Breadcrumbs from '@/app/components/Breadcrumbs/Breadcrumbs';
+import LinkButton from '@/app/components/Link/LinkButton';
+import Image from 'next/image';
+import styles from './style.module.css';
+
 
 export const metadata = {
     title: "Новости",
     description: "Описание новости",
 };
+
 export default async function NewsPage({ params }) {
     const { slug } = await params;
     const page = await getNewsBySlug(slug);
-    
+    const sanitizedContent = page.content || '';
+      
     if (!page) {
         notFound();
     }
 
-    return (
-        <>
-            <div>Динамический роути</div>
-            <div>Страница с слагом {slug}</div>
-            <div>{page ? JSON.stringify(page) : 'Событие не найдено'}</div>
-        </>
-    );
+   
+  return (
+    <div className='container'>
+      <Breadcrumbs title={page.title} />
+
+      <h1 className={styles.title}>{page.title}</h1>
+
+      <header className={styles.header}>
+
+        <div className={styles.data_wrapper}>
+          <Image
+            src={'/icons/calendar.svg'}
+            alt={"дата"}
+            width={25}
+            height={25}
+          />
+          <div className={styles.day}>{page.date_event.day_d}</div>
+          <div>
+            <p>{page.date_event.month_F[1]} {page.date_event.year_Y}</p>
+            <p className={styles.week_day}>{page.date_event.day_D[0]}</p>
+          </div>
+        </div>
+
+        <div className={styles.item}>
+          <Image
+            src={'/icons/clock.svg'}
+            alt={"время"}
+            width={25}
+            height={25}
+          />
+          <div className={styles.tag}>Время чтения: {page.time_to_read}</div>
+        </div>
+      </header>
+
+      <div className={styles.image_wrapper}>
+        <Image
+          src={page.thumbnail ? page.thumbnail : '/placeholder/placeholder.png'}
+          alt={page.title}
+          width={1460}
+          height={723}
+          className={styles.image}
+        />
+      </div>
+
+      <div
+        className={styles.content}
+        dangerouslySetInnerHTML={{ __html: sanitizedContent }}
+      ></div>
+
+    </div>
+  );
 }
 
 
