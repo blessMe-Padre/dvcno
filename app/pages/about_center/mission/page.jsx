771cdda7efe0b231dd2174a14664d9fcce2009bd
kkirrin <khlebnikov-2016@mail.ru>
2024-12-19 13:17:38 +1000

    feat: работа за 19.12

--- app/pages/about_center/mission/page.jsx
+++ app/pages/about_center/mission/page.jsx
@@ -1,7 +1,11 @@
 import styles from './style.module.css';
-import { Breadcrumbs } from '@/app/components';
+import { Breadcrumbs, SlideMain } from '@/app/components';
 
-import missionBg from '@/public/about_center/mission_bg.png';
+import missionBg2 from '@/public/about_center/mission-bg-2.png';
+import missionBgSmall from '@/public/about_center/mission-bg-2-s.png'
+
+import decor from '@/public/about_center/decor_mission.svg';
+import icon from '@/public/about_center/icon.svg';
 
 import Image from 'next/image';
 
@@ -11,12 +15,66 @@ export const metadata = {
 }
 
 export default function Page() { 
+
     return (
         <section className={styles.section}>
             <div className={`${styles.bg} container relative`}>
                 <Breadcrumbs title={"Миссия"} />
-                <h2 className={styles.title}>Миссия</h2>
-               
+
+                <div className={styles.wrapper}>
+                    <div className={styles.wrapper_title}>
+                        <h2 className={styles.title}>Миссия ДВЦНО </h2>
+                        <Image 
+                            src={decor}
+                            width={400}
+                            height={20}
+                            alt='decor'
+                            className={styles.decor}
+                        />
+                        <Image 
+                                src={icon}
+                                width={400}
+                                height={150}
+                                alt='icon'
+                                className={styles.icon}
+                        />
+                    </div>
+
+                    
+                    <div className={styles.info}>
+                        <div className={styles.info_content}>
+
+                            <p className={styles.text}>
+                                <span style={{ fontWeight: '700'}}> МИССИЯ ДВЦНО </span>- удовлетворение образовательных потребностей и запросов государства и социума в получении доступного и качественного образования детей, взрослых и талантливой молодежи, соответствующего требованиям инновационного социально ориентированного развития Приморского края.
+                            </p>
+
+                            <p className={styles.text}>
+                                <span style={{ fontWeight: '700'}}> Инновационное оказание услуг</span> в области образования от дошкольного до профессионального, разностороннее развитие личностей, их адаптации к жизни в обществе, стимулирование к познанию и творчеству, подготовка высокообразованных, конкурентоспособных, ориентированных на саморазвитие и непрерывное профессиональное образование специалистов
+                            </p>
+                        </div>
+
+                        <div className={styles.bg2}>
+                            <Image
+                                className={styles.bg2_img}
+                                src={missionBg2}
+                                width={620}
+                                height={680}
+                                alt=''
+                            />
+
+                            <Image
+                                className={styles.bg3_img}
+                                src={missionBgSmall}
+                                width={335}
+                                height={435}
+                                alt=''
+                            />
+
+
+                        </div>
+                    </div>
+                        
+                </div>
             </div>
         </section>
     )
