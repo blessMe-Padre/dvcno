14fb3c13490b2758480460c2b168943a8d962e3c
kkirrin <khlebnikov-2016@mail.ru>
2024-12-19 16:39:53 +1000

    feat: страница История

--- app/pages/about_center/history/page.jsx
+++ app/pages/about_center/history/page.jsx
@@ -1,6 +1,9 @@
 import styles from './style.module.css';
 import { Breadcrumbs } from '@/app/components';
 
+import decor from '@/public/about_center/decor2.svg';
+import Image from 'next/image';
+
 export const metadata = {
     title: "ДВЦНО | История",
     description: "Дальневосточный центр непрерывного образования",
@@ -8,9 +11,44 @@ export const metadata = {
 
 export default function Page() { 
     return (
-        <div>
-            <Breadcrumbs title={"История"} />
-            <h1>История</h1>
-        </div>
+        <>
+            <section>
+                <div className='container'>
+                    <Breadcrumbs
+                        slug={'О центре'}
+                        link={'pages/about_center'}
+                        title={'История'}
+                        />
+                    <div className={styles.title_wrapper}>
+                        <h2 className={styles.title}>История АНПОО «ДВЦНО»</h2>
+
+                        <Image 
+                            className={styles.decor}
+                            src={decor}
+                            width={300}
+                            height={100}
+                            alt=''
+                            />
+                    </div>
+                </div>
+            </section>
+
+            <section className={styles.list}>
+                <div className='container'>
+                    <ul className={styles.list_wrapper}>
+                        <li className={styles.list_content}>
+                            1
+                        </li>
+
+                        <li className={styles.list_content}>
+                            <div className={styles.list_content_wrapper}>
+                                <div className={styles.list_content_year}>2004 год</div>
+                                <p className={styles.list_content_desc}>Создание «Института непрерывного образования»</p>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </section>
+        </>
     )
 }
