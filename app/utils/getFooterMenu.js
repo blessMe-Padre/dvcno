3d0343b40fac89f5380f2d99958ce87b4c7d55d7
Сергей Субботин <zarodiny@yandex.ru>
2025-01-10 14:49:54 +1000

    feat: добавляет бургер меню

--- app/utils/getFooterMenu.js
+++ app/utils/getFooterMenu.js
@@ -1,7 +1,7 @@
-const getPopupMenu = async () => {
+const getFooterMenu = async () => {
     try {
         // http://localhost:3000/api/popup-menu/
-        const res = await fetch('http://134.0.118.139/api/v1/menus/menu_popup?lang=ru', {
+        const res = await fetch('http://134.0.118.139/api/v1/menus/menu_footer?lang=ru', {
             next: { revalidate: 10 }
         });
         if (!res.ok) {
@@ -15,4 +15,4 @@ const getPopupMenu = async () => {
     }
 };
 
-export default getPopupMenu;
+export default getFooterMenu;
