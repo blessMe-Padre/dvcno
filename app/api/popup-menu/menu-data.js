af84e45d7ed7f75d4fa9d78feb260286cdd25465
Alena.Lialiushkina <pluto_16@mail.ru>
2025-04-18 14:21:24 +1000

    fix: пункты меню в соответствии с макетом,  добавляет страницы к пунктам меню

--- app/api/popup-menu/menu-data.js
+++ app/api/popup-menu/menu-data.js
@@ -1,58 +1,62 @@
 export const popupMenuData = [
-    {
-        title: "Информационная безопасность",
-        link: "/info-security",
-    },
-    {
-        title: 'Локальные акты',
-        link: '/info-security/lokalnye-akty'
-    },
-    {
-        title: "О центре",
-        link: "/about_center",
-    },
-    {
-        title: "ГИА",
-        link: "#",
-    },
-    {
-        title: "ОРКСЭ/ОДКНР",
-        link: "#",
-    },
-    {
-        title: "Вакансии",
-        link: "#",
-    },
-    {
-        title: "Электронное обучение",
-        link: "#",
-    },
-    {
-        title: "Противодействие коррупции",
-        link: "#",
-    },
-    {
-        title: "Противодействие идеологии терроризма и экстремизма",
-        link: "#",
-    },
-    {
-        title: "Обработка персональных данных",
-        link: "#",
-    },
-    {
-        title: "Электронное обучение",
-        link: "#",
-    },
-    {
-        title: "Оплата услуг банковской картой",
-        link: "#",
-    },
-    {
-        title: "Сервисы",
-        link: "#",
-    },
-    {
-        title: "Контакты",
-        link: "#",
-    },
+  {
+    title: "Сведения об образовательной организации",
+    link: "/info",
+  },
+  {
+    title: "Обучение",
+    link: "/education",
+  },
+  {
+    title: "О центре",
+    link: "/about_center",
+  },
+  {
+    title: "Информационная безопасность",
+    link: "/info-security",
+  },
+  {
+    title: "ГИА",
+    link: "/gia",
+  },
+  {
+    title: "ОРКСЭ/ОДКНР",
+    link: "/orkse-odknr",
+  },
+  {
+    title: "Результаты СОУТ",
+    link: "/results_sout",
+  },
+  {
+    title: "Вакансии",
+    link: "/vacancies",
+  },
+  {
+    title: "Противодействие коррупции",
+    link: "/resistance_to_damage",
+  },
+  {
+    title: "Противодействие идеологии терроризма и экстремизма",
+    link: "/resistance_to_damage_2",
+  },
+  {
+    title: "Обработка персональных данных",
+    link: "/processing-of-personal-data",
+  },
+  {
+    title: "Электронное обучение",
+    link: "#",
+  },
+  {
+    title: "Оплата услуг банковской картой",
+    link: "#",
+  },
+  {
+    title: "Сервисы",
+    link: "#",
+  },
+  {
+    title: "Контакты",
+    link: "/contacts",
+  },
 ];
