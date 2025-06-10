a88ffeee9b8568143daaa4c4048d0f4516162ef8
Сергей Субботин <zarodiny@yandex.ru>
2025-06-10 11:01:43 +1000

    feat: добавляет traning api

---
+++ app/(pages)/education/PageContent.jsx
@@ -0,0 +1,46 @@
+'use client'
+
+import Card from "@/app/components/Cards/Card/Card";
+import styles from "./style.module.css";
+import Breadcrumbs from "@/app/components/Breadcrumbs/Breadcrumbs";
+import useLangStore from '@/app/store/languageStore';
+
+const PageContent = ({ data }) => {
+    const { lang } = useLangStore();
+    const contentPage = data?.sections;
+    const listBlock = data?.sections?.main[2]?.content;
+
+    const languages = {
+        ru: 'Обучение',
+        en: 'Training',
+        ch: '教育'
+    };
+
+    return (
+        <>
+            <Breadcrumbs title={languages[lang]} />
+            <h2 className={`title ${styles.title}`}>{contentPage?.main[0]?.content?.[lang]?.[0]}</h2>
+            <div className={styles.description}
+                dangerouslySetInnerHTML={{ __html: contentPage?.main[1]?.content?.[lang]?.[0] }}
+            />
+
+            <ul className={styles.list}>
+                {data && listBlock[lang] && listBlock[lang].length > 0 ? (
+                    listBlock[lang].map((item) => (
+                        <li key={item.id}>
+                            <Card
+                                image={`${process.env.NEXT_PUBLIC_API_SERVER}${item.img}`}
+                                description={item.text}
+                                link={`education/${item.url}`}
+                            />
+                        </li>
+                    ))
+                ) : (
+                    <p className="span-error-message">Ошибка в получении данных</p>
+                )}
+            </ul>
+        </>
+    )
+}
+
+export default PageContent
--- app/(pages)/education/page.jsx
+++ app/(pages)/education/page.jsx
@@ -1,8 +1,6 @@
-import Card from "@/app/components/Cards/Card/Card";
-import styles from "./style.module.css";
-import Breadcrumbs from "@/app/components/Breadcrumbs/Breadcrumbs";
-import fetchApiData from "@/app/utils/fetchApiData";
 
+import fetchApiServerData from "@/app/utils/fetchApiServerData";
+import PageContent from "./PageContent";
 
 export const metadata = {
     title: "ДВЦНО | Обучение",
@@ -18,28 +16,8 @@ export const metadata = {
 };
 
 export default async function Page() {
-    const data = await fetchApiData('education');
+    const data = await fetchApiServerData('pages/training');
     return (
-        <>
-            <Breadcrumbs title={"Обучение"} />
-            <h2 className={`title ${styles.title}`}>Обучение</h2>
-            <p className={styles.description}>Дальневосточный центр непрерывного образования (ДВЦНО) предлагает полный цикл образовательных услуг. От дошкольного образования до получения специальности (профессиональное обучение) и профессиональной переподготовки</p>
-
-            <ul className={styles.list}>
-                {data && data.length > 0 ? (
-                    data.map((item) => (
-                        <li key={item.id}>
-                            <Card
-                                image={item.image}
-                                description={item.description}
-                                link={`education/${item.slug}`}
-                            />
-                        </li>
-                    ))
-                ) : (
-                    <p className="span-error-message">Данные отсутствуют.</p>
-                )}
-            </ul>
-        </>
+        <PageContent data={data.data} />
     )
 }
