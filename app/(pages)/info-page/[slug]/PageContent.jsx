b6dd6dcd31c7852dc21f80fd6c8368ed1008a842
Сергей Субботин <zarodiny@yandex.ru>
2025-09-09 12:12:40 +1000

    fix: обновляет логику получения данных и стили для страницы информации

--- app/(pages)/info-page/[slug]/PageContent.jsx
+++ app/(pages)/info-page/[slug]/PageContent.jsx
@@ -6,21 +6,14 @@ import Breadcrumbs from '@/app/components/Breadcrumbs/Breadcrumbs';
 import { insertSafeContent } from '@/app/utils/insertSafeContent';
 import styles from './style.module.css';
 
-const content = `<p>content</p>`; // TODO: change to content from API
-
 const PageContent = ({ data }) => {
-
-    console.log(data);
     const { lang } = useLangStore();
 
     return (
         <div className='container'>
             <Breadcrumbs title={data?.title?.[lang]} />
             <h1>{data?.title?.[lang]}</h1>
-            <div>
-                {insertSafeContent(content)}
-            </div>
-
+            <div className={styles.content} dangerouslySetInnerHTML={{ __html: data?.sections?.main?.[2]?.content?.[lang] }} />
         </div>
     )
 }
--- app/(pages)/info-page/[slug]/page.jsx
+++ app/(pages)/info-page/[slug]/page.jsx
@@ -1,27 +1,24 @@
 import PageContent from "./PageContent";
 import fetchApiServerData from "@/app/utils/fetchApiServerData";
 
-
 export const revalidate = 10;
 
 export async function generateMetadata({ params }) {
     const { slug } = params;
+    const result = await fetchApiServerData(`pages/info-security`);
+
     return {
         title: `ДВЦНО | ${slug}`,
         description: slug,
     };
 }
 
-const fakeSlug = 'info-security';
-
 export default async function Page() {
-    const result = await fetchApiServerData(`pages/${fakeSlug}`);
+    const result = await fetchApiServerData(`pages/info-security`);
     const data = result?.data;
 
     return (
-        <>
-            <PageContent data={data} />
-        </>
+        <PageContent data={data} />
     )
 }
 
